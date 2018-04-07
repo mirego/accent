8fe2b10e73801ee2301e3e4495f802e2068d0706
Simon Prévost <sprevost@mirego.com>
2018-04-07 17:31:59 -0400

    Add tests for error view module

--- lib/web/views/error_view.ex
+++ lib/web/views/error_view.ex
@@ -1,10 +1,44 @@
 defmodule Accent.ErrorView do
+  @moduledoc """
+  Render common errors for JSON or HTML format
+
+  ## Examples
+
+    iex> Accent.ErrorView.render("400.json", %{reason: "test"})
+    %{error: "Bad request", message: "test"}
+    iex> Accent.ErrorView.render("400.json", %{reason: %{complex: "data"}})
+    %{error: "Bad request", message: "-"}
+    iex> Accent.ErrorView.render("400.json", %{reason: %Ecto.Query.CastError{}})
+    %{error: "Bad request", message: "Bad argument type cast"}
+
+    iex> Accent.ErrorView.render("404.json", %{reason: %Phoenix.Router.NoRouteError{}})
+    %{error: "Not found", message: "Route not found"}
+    iex> Accent.ErrorView.render("404.json", %{reason: %Ecto.NoResultsError{}})
+    %{error: "Not found", message: "Resource not found"}
+    iex> Accent.ErrorView.render("404.json", %{reason: "test"})
+    %{error: "Not found", message: "test"}
+    iex> Accent.ErrorView.render("404.json", %{reason: %{complex: "data"}})
+    %{error: "Not found", message: "-"}
+
+    iex> Accent.ErrorView.render("500.json", %{})
+    %{error: "Internal error", message: "An error occured, someone as been notified"}
+
+    iex> Accent.ErrorView.render("404.html", %{})
+    "Page not found"
+
+    iex> Accent.ErrorView.render("500.html", %{})
+    "Server internal error"
+    iex> Accent.ErrorView.template_not_found("index.html", %{})
+    "Server internal error"
+  """
+
   use Phoenix.View, root: "lib/web/templates"
 
   def render("400.json", %{reason: reason}) do
     message =
       case reason do
         %Ecto.Query.CastError{} -> "Bad argument type cast"
+        error when is_binary(error) -> error
         _ -> "-"
       end
 
@@ -19,6 +53,7 @@ defmodule Accent.ErrorView do
       case reason do
         %Phoenix.Router.NoRouteError{} -> "Route not found"
         %Ecto.NoResultsError{} -> "Resource not found"
+        error when is_binary(error) -> error
         _ -> "-"
       end
 
