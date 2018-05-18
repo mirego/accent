f2e6922b34a042fd216e1207d3443e4234baed2a
Nicolas Boisvert <nicklay@me.com>
2018-05-18 16:53:31 -0400

    laravel - Fix Credo linting

--- test/langue/laravel/expectation_test.exs
+++ test/langue/laravel/expectation_test.exs
@@ -19,10 +19,10 @@ defmodule LangueTest.Formatter.Laravel.Expectation do
 
     def entries do
       [
-        %Entry{comment: "", index: 1, key: "some_double_quoted_key", value: "Some double quoted value" },
-        %Entry{comment: "", index: 1, key: "another_double_quoted_key", value: "A single quoted value" },
-        %Entry{comment: "", index: 1, key: "some_single_quoted_key", value: "With a double quoted value" },
-        %Entry{comment: "", index: 1, key: "another_single_quoted_key", value: "With a single quoted value" },
+        %Entry{comment: "", index: 1, key: "some_double_quoted_key", value: "Some double quoted value"},
+        %Entry{comment: "", index: 1, key: "another_double_quoted_key", value: "A single quoted value"},
+        %Entry{comment: "", index: 1, key: "some_single_quoted_key", value: "With a double quoted value"},
+        %Entry{comment: "", index: 1, key: "another_single_quoted_key", value: "With a single quoted value"},
       ]
     end
   end
