ea2c216fe005d604a6665bbd3bbd083a0ceeb58d
Nicolas Boisvert <nicklay@me.com>
2018-05-18 14:22:45 -0400

    laravel - Done serialization, regex must be revised

--- lib/langue/formatter/laravel/serializer.ex
+++ lib/langue/formatter/laravel/serializer.ex
@@ -1,8 +1,24 @@
 defmodule Langue.Formatter.Laravel.Serializer do
   @behaviour Langue.Formatter.Serializer
 
-  def serialize(%{entries: entries, locale: locale}) do
-    render = nil
-    %Langue.Formatter.SerializerResult{render: render}
+  def serialize(%{entries: entries}) do
+    render =
+      entries
+      |> Enum.map(&map_entry(&1))
+
+    %Langue.Formatter.SerializerResult{render: wrap_values(render)}
+  end
+
+  def map_entry(%Langue.Entry{key: key, value: value}) do
+    "\n\t\"#{key}\" => \"#{value}\","
+  end
+
+  def wrap_values(values) do
+    """
+    <?php
+
+    return [#{values}
+    ];
+    """
   end
 end
