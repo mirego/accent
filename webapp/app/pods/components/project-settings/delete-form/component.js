ce6ffe67a13b1a9df671afe450c7d1e2d91decff
Louis-Michel Couture <louismichel.couture@hooktstudios.com>
2018-06-27 21:12:32 -0400

    Confirm before deleting a project.

--- webapp/app/pods/components/project-settings/delete-form/component.js
+++ webapp/app/pods/components/project-settings/delete-form/component.js
@@ -1,11 +1,18 @@
 import Component from '@ember/component';
+import {inject as service} from '@ember/service';
 
 // Attributes
 // project: Object <project>
 // onSubmit: Function
 export default Component.extend({
+  i18n: service(),
+
   actions: {
     deleteProject() {
+      /* eslint-disable no-alert */
+      if (!window.confirm(this.i18n.t('components.project_settings.delete_form.delete_project_confirm'))) return;
+      /* eslint-enable no-alert */
+
       this.onSubmit();
     }
   }
