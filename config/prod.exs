41c5a2f781e2ed2cc5c7f07f8e5aa48f78ff5639
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2018-12-03 06:27:40 -0800

    Make accent work with phoenix.

--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,13 @@
 use Mix.Config
 
-config :accent, Accent.Endpoint, check_origin: false
 config :accent, dummy_provider_enabled: false
+
+config :accent, Accent.Endpoint, check_origin: false,
+  http: [port: {:system, "PORT"}],
+  url: [host: "localhost", port: {:system, "PORT"}], # This is critical for ensuring web-sockets properly authorize.
+  cache_static_manifest: "priv/static/manifest.json",
+  server: true,
+  root: ".",
+  version: Application.spec(:accent, :vsn)
 
 config :logger, level: :info
