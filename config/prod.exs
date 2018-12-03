c1ece7532637c940a5a28af7db93adc777260a40
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2018-12-03 06:56:04 -0800

    cache_manifest is the new manifest filename.

--- config/prod.exs
+++ config/prod.exs
@@ -5,7 +5,7 @@ config :accent, dummy_provider_enabled: false
 config :accent, Accent.Endpoint, check_origin: false,
   http: [port: {:system, "PORT"}],
   url: [host: "localhost", port: {:system, "PORT"}], # This is critical for ensuring web-sockets properly authorize.
-  cache_static_manifest: "priv/static/manifest.json",
+  cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   root: ".",
   version: Application.spec(:accent, :vsn)
