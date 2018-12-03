483fae3a9094805311049e89952dd45e7e82a8b5
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2018-12-03 05:13:46 -0800

    Lower elixir requirement.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Accent.Mixfile do
     [
       app: :accent,
       version: "0.0.1",
-      elixir: "~> 1.7.0",
+      elixir: "~> 1.6.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
       build_embedded: Mix.env() == :prod,
