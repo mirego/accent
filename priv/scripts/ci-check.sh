5cf5db19c4da8c7fb462ba67cf39c05e9be9a4db
Simon Prévost <sprevost@mirego.com>
2018-12-08 16:09:29 -0500

    Remove emoji from ci-check script

--- priv/scripts/ci-check.sh
+++ priv/scripts/ci-check.sh
@@ -1,37 +1,28 @@
 #!/usr/bin/env sh
 
 error_status=0
-success_emoji=✅
-fail_emoji=❌
+
+RED='\033[0;31m'
+RED_BOLD='\033[1;31m'
+GREEN='\033[0;32m'
+GREEN_BOLD='\033[1;32m'
+YELLOW='\033[0;33m'
+NO_COLOR='\033[0m'
+
+header() {
+  echo "\n\n${YELLOW}▶ $1${NO_COLOR}"
+}
 
 run() {
   eval "${@}"
   last_exit_status=${?}
 
   if [ ${last_exit_status} -ne 0 ]; then
-    echo "Something went wrong. Program exited with ${last_exit_status}"
+    echo "\n${RED}↳ Something went wrong. Program exited with ${last_exit_status} ✘${NO_COLOR}"
     error_status=${last_exit_status}
   else
-    echo ${success_emoji}
+    echo "${GREEN}↳ Passed ✔${NO_COLOR}"
   fi
-}
-
-separator() {
-  index=0
-
-  while [ $index -le "${1}" ]; do
-    printf "–"
-    index=$((index + 1))
-  done
-
-  echo ""
-}
-
-header() {
-  chrlen=$((${#1} + 10))
-  separator ${chrlen}
-  echo "     $1     "
-  separator ${chrlen}
 }
 
 header "API tests…"
@@ -56,9 +47,9 @@ header "Webapp tests…"
 run npm --prefix webapp test
 
 if [ ${error_status} -ne 0 ]; then
-  header "${fail_emoji}   Something went wrong. Please fix it before committing."
+  echo "\n\n${YELLOW}▶▶ One of the checks ${RED_BOLD}failed${YELLOW}. Please fix it before committing.${NO_COLOR}"
 else
-  header "${success_emoji}   Everything looks good!"
+  echo "\n\n${YELLOW}▶▶ All checks ${GREEN_BOLD}passed${YELLOW}!${NO_COLOR}"
 fi
 
 exit $error_status
