31bbdabc55ab9fabb79821022727ee981180257e
Simon Prévost <sprevost@mirego.com>
2019-01-10 18:50:49 -0500

    Add maintenance page when ember build runs in the background on deploy

--- lib/web/controllers/webapp_controller.ex
+++ lib/web/controllers/webapp_controller.ex
@@ -1,11 +1,38 @@
 defmodule Accent.WebAppController do
   use Plug.Builder
 
+  import Phoenix.Controller, only: [put_view: 2, render: 2]
+
+  alias Accent.WebappView
+
+  plug(:ensure_file_exists)
   plug(:index)
 
+  @doc """
+  Serves the static app built from the webapp folder.
+
+  Since the build operation is done asynchronously on deploy, we need a maintenance page until
+  the index.html is available.
+  """
   def index(conn, _) do
     conn
     |> put_resp_header("content-type", "text/html; charset=utf-8")
-    |> Plug.Conn.send_file(200, Application.app_dir(:accent, "priv/static/webapp/index.html"))
+    |> send_file(200, conn.assigns[:file])
+  end
+
+  def ensure_file_exists(conn, _) do
+    file = Application.app_dir(:accent, "priv/static/webapp/index.html")
+
+    file
+    |> File.read()
+    |> case do
+      {:error, _} ->
+        conn
+        |> put_view(WebappView)
+        |> render("maintenance.html")
+
+      _ ->
+        assign(conn, :file, file)
+    end
   end
 end
