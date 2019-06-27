a800fad039a6f6c46ce4836a505d1457deda1b6b
Simon Prévost <sprevost@mirego.com>
2019-06-27 15:36:01 -0400

    v1.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Accent.Mixfile do
   def project do
     [
       app: :accent,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
