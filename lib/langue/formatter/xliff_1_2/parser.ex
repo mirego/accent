5daf453e2f50f5da08e67c97ed53455a0d7a7bb4
Karol Wojtaszek <karol@appunite.com>
2019-07-26 16:19:01 +0200

    Add initial support for xliff generated by xcode

--- lib/langue/formatter/xliff_1_2/parser.ex
+++ lib/langue/formatter/xliff_1_2/parser.ex
@@ -4,38 +4,81 @@ defmodule Langue.Formatter.XLIFF12.Parser do
   alias Langue.Entry
   alias Langue.Utils.Placeholders
 
+  require IEx
+
   def parse(%{render: render}) do
     render
-    |> :erlsom.simple_form()
+    |> :erlsom.simple_form([{:nameFun, fn(name, _namespace, _prefix) -> name end}])
     |> case do
-      {:ok, {'file', _attributes, [{'body', _, body}]}, _} ->
-        entries =
-          body
-          |> Enum.with_index(1)
-          |> Enum.map(&parse_line/1)
-          |> Enum.reject(&is_nil/1)
-          |> Placeholders.parse(Langue.Formatter.XLIFF12.placeholder_regex())
-
-        %Langue.Formatter.ParserResult{entries: entries}
-
+      {:ok, {'file', _attributes, _body} = file, _} ->
+        %Langue.Formatter.ParserResult{entries: process_file(file)}
+      {:ok, {'xliff', _attributes, body}, _} ->
+        %Langue.Formatter.ParserResult{entries: process_xliff(body, [])}
       _ ->
         Langue.Formatter.ParserResult.empty()
     end
   end
 
-  defp parse_line({{'trans-unit', [{'id', key}], [{'source', [], [source]}, {'target', [], [value]}]}, index}) do
+  defp generate_entry(filename, key, value, source, comment, index) do
     key = IO.chardata_to_string(key)
     value = IO.chardata_to_string(value)
     source = IO.chardata_to_string(source)
+    comment = case comment do
+      nil -> nil
+      _ -> IO.chardata_to_string(comment)
+    end
 
     %Entry{
       value: value,
       master_value: source,
       value_type: Langue.ValueType.parse(value),
+      comment: comment,
       key: key,
-      index: index
+      index: index,
+      file: filename
     }
   end
 
-  defp parse_line(_), do: nil
+  defp parse_line(filename, {{'trans-unit', [{'id', key}], [{'source', [], [source]}, {'target', [], [value]}]}, index}), do: generate_entry(filename, key, value, source, nil, index)
+  defp parse_line(filename, {{'trans-unit', [{'id', key}], [{'source', [], [source]}, {'target', [], [value]}, {'note', [], [note]}]}, index}), do: generate_entry(filename, key, value, source, note, index)
+
+  defp parse_line(_, _), do: nil
+
+  defp fetch_filename(nil), do: nil
+  defp fetch_filename(attributes) do
+    {'original', file} =  Enum.find(attributes, {'original', nil}, fn x -> case x do
+      {'original', _} -> true
+      _ -> false
+    end
+    end)
+
+    file
+  end
+
+
+  defp process_file_entry(body, attributes) do
+    filename = fetch_filename(attributes)
+    entries =
+      body
+      |> Enum.with_index(1)
+      |> Enum.map(fn x -> parse_line(filename, x) end)
+      |> Enum.reject(&is_nil/1)
+      |> Placeholders.parse(Langue.Formatter.XLIFF12.placeholder_regex())
+
+    entries
+  end
+
+  defp process_file({'file', attributes, [{'body', _, body}]}), do: process_file_entry(body, attributes)
+  defp process_file({'file', attributes, [{'header', _, _header}, {'body', _, body}]}), do: process_file_entry(body, attributes)
+
+  defp process_xliff([file | rest], accumulator) do
+    entries = process_file(file)
+
+    process_xliff(rest, accumulator ++ entries)
+  end
+
+  defp process_xliff([], accumulator) do
+    accumulator
+  end
+
 end
--- test/langue/xliff_1_2/expectation_test.exs
+++ test/langue/xliff_1_2/expectation_test.exs
@@ -28,4 +28,51 @@ defmodule LangueTest.Formatter.XLIFF12.Expectation do
       ]
     end
   end
+
+  defmodule MultipleFile do
+    use Langue.Expectation.Case
+
+    def render do
+      """
+      <?xml version="1.0" encoding="UTF-8"?>
+      <xliff xmlns="urn:oasis:names:tc:xliff:document:1.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="1.2" xsi:schemaLocation="urn:oasis:names:tc:xliff:document:1.2 http://docs.oasis-open.org/xliff/v1.2/os/xliff-core-1.2-strict.xsd">
+        <file original="XApp/Base.lproj/LaunchScreen.storyboard" source-language="en" target-language="fr" datatype="plaintext">
+          <header>
+            <tool tool-id="com.apple.dt.xcode" tool-name="Xcode" tool-version="10.2.1" build-num="10E1001"/>
+          </header>
+          <body>
+            <trans-unit id="greeting">
+              <source>hello</source>
+              <target>bonjour</target>
+              <note>Class = "UILabel"; text = "XApp"; ObjectID = "hello";</note>
+            </trans-unit>
+          </body>
+        </file>
+        <file original="XApp/Base.lproj/Main.storyboard" source-language="en" target-language="fr" datatype="plaintext">
+          <header>
+            <tool tool-id="com.apple.dt.xcode" tool-name="Xcode" tool-version="10.2.1" build-num="10E1001"/>
+          </header>
+          <body>
+            <trans-unit id="goodbye">
+              <source>Bye bye</source>
+              <target>À la prochaine</target>
+            </trans-unit>
+            <trans-unit id="hi">
+              <source>Hi</source>
+              <target>Salut</target>
+            </trans-unit>
+          </body>
+        </file>
+      </xliff>
+      """
+    end
+
+    def entries do
+      [
+        %Entry{key: "greeting", value: "bonjour", master_value: "hello", comment: "Class = \"UILabel\"; text = \"XApp\"; ObjectID = \"hello\";", index: 1, file: "XApp/Base.lproj/LaunchScreen.storyboard"},
+        %Entry{key: "goodbye", value: "À la prochaine", master_value: "Bye bye", index: 2, file: "XApp/Base.lproj/Main.storyboard"},
+        %Entry{key: "hi", value: "Salut", master_value: "Hi", index: 3, file: "XApp/Base.lproj/Main.storyboard"}
+      ]
+    end
+  end
 end
