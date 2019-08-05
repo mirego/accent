34678067c067391127d2b7abcf72320774815acc
Simon Prévost <sprevost@mirego.com>
2019-08-05 12:50:30 -0400

    Updates

--- webapp/app/pods/logged-in/project/edit/service-integrations/route.js
+++ webapp/app/pods/logged-in/project/edit/service-integrations/route.js
@@ -1,10 +1,13 @@
 import {get} from '@ember/object';
+import {inject as service} from '@ember/service';
 import Route from '@ember/routing/route';
 import ApolloRoute from 'accent-webapp/mixins/apollo-route';
 
 import projectServiceIntegrationsQuery from 'accent-webapp/queries/project-service-integrations';
 
 export default Route.extend(ApolloRoute, {
+  routeParams: service(),
+
   model(_params, transition) {
     return this.graphql(projectServiceIntegrationsQuery, {
       props: data => ({
