c6ee149163b74cb664084234bc137a6c5251e3d7
romarickb <rboley@mirego.com>
2020-05-01 09:18:58 -0400

    Collaborator's email must be unique for a projet (#169)
    * Collaborator's email must be unique for a projet. Can't add multiple time the same collaborator in a project
    
    * Update priv/repo/migrations/20200430174230_unique_collaborator_email.exs
    
    Co-authored-by: Rémi Prévost <remi@exomel.com>
    
    Co-authored-by: Simon Prévost <sprevost@mirego.com>
    Co-authored-by: Rémi Prévost <remi@exomel.com>

---
+++ priv/repo/migrations/20200430174230_unique_collaborator_email.exs
@@ -0,0 +1,24 @@
+defmodule Accent.Repo.Migrations.UniqueCollaboratorEmail do
+  use Ecto.Migration
+
+  def change do
+    execute(
+      """
+      WITH oldest_email_collaborators AS (
+       SELECT email, MIN(inserted_at) as inserted_at
+       FROM collaborators
+       GROUP BY email
+      )
+      DELETE FROM collaborators
+      WHERE inserted_at > (
+       SELECT oldest_email_collaborators.inserted_at
+       FROM oldest_email_collaborators
+       WHERE collaborators.email = oldest_email_collaborators.email
+      )
+      """,
+      ""
+    )
+
+    create(unique_index(:collaborators, [:email, :project_id]))
+  end
+end
