ae755afa3952a87d013139f1f1e6e62ce3488c4b
Simon Prévost <sprevost@mirego.com>
2020-05-04 13:15:11 -0400

    Add scoped collaborators uniqueness migration

--- priv/repo/migrations/20200430174230_unique_collaborator_email.exs
+++ priv/repo/migrations/20200430174230_unique_collaborator_email.exs
@@ -2,6 +2,38 @@ defmodule Accent.Repo.Migrations.UniqueCollaboratorEmail do
   use Ecto.Migration
 
   def change do
+    execute(
+      """
+      WITH oldest_email_collaborators AS (
+        SELECT
+            email,
+            project_id,
+            MIN(inserted_at) AS
+            inserted_at
+        FROM
+            collaborators
+        GROUP BY
+            email,
+            project_id
+        HAVING
+            ARRAY_LENGTH(ARRAY_AGG(project_id), 1) > 1
+      )
+      DELETE FROM
+          collaborators
+      WHERE
+          inserted_at > (
+        SELECT
+            oldest_email_collaborators.inserted_at
+        FROM
+            oldest_email_collaborators
+        WHERE
+            collaborators.email = oldest_email_collaborators.email
+            AND collaborators.project_id = oldest_email_collaborators.project_id
+      )
+      """,
+      ""
+    )
+
     create(unique_index(:collaborators, [:email, :project_id]))
   end
 end
