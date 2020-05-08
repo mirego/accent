6769b4928b19dd0f9c601c2c54b40657d7556103
Simon Prévost <sprevost@mirego.com>
2020-05-08 07:31:07 -0400

    Use dev tag in dev env for version tag in footer

--- webapp/app/pods/components/application-footer/component.ts
+++ webapp/app/pods/components/application-footer/component.ts
@@ -4,13 +4,9 @@ import config from 'accent-webapp/config/environment';
 export default class ApplicationFooter extends Component {
   // The version is replaced at runtime when served by the API.
   // If the webapp is not served by the API (like in development),
-  // the version tag will show up as empty.
+  // the version tag will show up as 'dev'.
   get version() {
-    if (config.version === '__VERSION__') {
-      return '';
-    } else {
-      return config.version;
-    }
+    return config.version === '__VERSION__' ? 'dev' : config.version;
   }
 
   toggleDark() {
