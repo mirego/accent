70926dd492a11ec2083ad3f542656842be4077e4
Simon Prévost <sprevost@mirego.com>
2023-01-18 12:54:52 -0500

    Fix Rails formatter

--- lib/langue/formatter/rails/rails.ex
+++ lib/langue/formatter/rails/rails.ex
@@ -1,9 +1,20 @@
 defmodule Langue.Formatter.Rails do
-  use Langue.Formatter,
-    id: "rails_yml",
-    display_name: "Rails YAML",
-    extension: "yml"
+  @enabled Code.ensure_loaded?(:fast_yaml)
 
-  def enabled?, do: Code.ensure_loaded?(:fast_yaml)
+  if @enabled do
+    use Langue.Formatter,
+      id: "rails_yml",
+      display_name: "Rails YAML",
+      extension: "yml",
+      parser: Langue.Formatter.Rails.Parser,
+      serializer: Langue.Formatter.Rails.Serializer
+  else
+    use Langue.Formatter,
+      id: "rails_yml",
+      display_name: "Rails YAML",
+      extension: "yml"
+  end
+
+  def enabled?, do: @enabled
   def placeholder_regex, do: ~r/%{[^}]*}/
 end
