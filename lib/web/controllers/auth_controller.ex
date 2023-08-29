4483fe03c43b5017a222ca5c489e7025b35418dc
Simon Prévost <sprevost@mirego.com>
2023-08-29 14:39:06 -0400

    Fix compile time route ueberauth controller

--- lib/web/controllers/auth_controller.ex
+++ lib/web/controllers/auth_controller.ex
@@ -4,7 +4,7 @@ defmodule Accent.AuthController do
   alias Accent.UserRemote.Authenticator
   alias Ueberauth.Strategy.Helpers
 
-  plug(Ueberauth, base_path: "/auth")
+  plug(:ueberauth)
 
   def request(conn, _params) do
     redirect(conn, external: Helpers.callback_url(conn))
@@ -22,5 +22,9 @@ defmodule Accent.AuthController do
 
   def callback(conn, _) do
     redirect(conn, to: "/")
+  end
+
+  def ueberauth(conn, _) do
+    Ueberauth.call(conn, Ueberauth.init())
   end
 end
