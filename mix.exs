9b2e7b2f316ee6ad7458b8ed768303458ad0bb5c
dynamyc <balint.sebestyen2004@gmail.com>
2024-02-20 08:43:30 +0100

    Add ueberauth_oidc as dependency

--- mix.exs
+++ mix.exs
@@ -91,6 +91,7 @@ defmodule Accent.Mixfile do
       {:ueberauth_github, "~> 0.7"},
       {:ueberauth_discord, "~> 0.5"},
       {:ueberauth_auth0, "~> 2.0"},
+      {:ueberauth_oidc, "~> 1.0"}
 
       # Errors
       {:sentry, "~> 7.0"},
