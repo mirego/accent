6da29de0349e9ad41f26c3394512b8618485464e
Dániel Földi <dani@stefan.hu>
2024-04-19 21:08:18 +0000

    Add extraHeaders config option

--- cli/src/types/config.ts
+++ cli/src/types/config.ts
@@ -5,6 +5,7 @@ import {VersionConfig} from './version-config';
 export interface Config {
   apiUrl: string;
   apiKey: string;
+  extraHeaders?: Record<string, string>;
   project?: string | null;
   version?: VersionConfig;
   files: DocumentConfig[];
